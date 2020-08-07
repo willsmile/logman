98282b8fa5a46bf54f662516f77dac0bc9c04e01
Wei Chen <smile.v.chen@gmail.com>
2020-08-07 21:14:24 +0900

    Create unit test for post module

---
+++ spec/unit/post_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+RSpec.describe Logman::Post do
+  let(:today) { Date.new(2019, 0o4, 0o1) }
+  let(:format) { "%y%m%dDL.md" }
+  let(:filename) { Date.today.strftime(format) }
+
+  before do
+    allow(Date).to receive(:today).and_return(today)
+  end
+
+  describe '#esa' do
+    context 'towards an exist logfile' do
+      before do
+        FileUtils.touch(filename)
+      end
+      after do
+        FileUtils.rm_f(filename)
+      end
+
+      it 'should return a successful message when esa response is valid' do
+        response = Hashie::Mash.new(status: 201, body: {'number': 1})
+        mock_esa_client = double('Esa Client')
+        allow(mock_esa_client).to receive(:create_post).and_return(response)
+        allow(mock_esa_client).to receive(:update_post).and_return(true)
+        allow(Logman::Post).to receive(:esa_client).and_return(mock_esa_client)
+
+        log = Logman::LogObject.new
+        expect { Logman::Post.esa(log) }.to output("Post logfile #{filename} to esa successful.\n").to_stdout
+      end
+
+      it 'should return a unsuccessful message when esa response is valid but update post failed' do
+        response = Hashie::Mash.new(status: 201, body: {'number': 1})
+        mock_esa_client = double('Esa Client')
+        allow(mock_esa_client).to receive(:create_post).and_return(response)
+        allow(mock_esa_client).to receive(:update_post).and_return(false)
+        allow(Logman::Post).to receive(:esa_client).and_return(mock_esa_client)
+
+        log = Logman::LogObject.new
+        expect { Logman::Post.esa(log) }.to output("Post logfile #{filename} to esa unsuccessful.\n").to_stdout
+      end
+
+      it 'should return an error message when esa response is invalid' do
+        response = Hashie::Mash.new(status: 404)
+        mock_esa_client = double('Esa Client')
+        allow(mock_esa_client).to receive(:create_post).and_return(response)
+        allow(mock_esa_client).to receive(:update_post).and_return(false)
+        allow(Logman::Post).to receive(:esa_client).and_return(mock_esa_client)
+
+        log = Logman::LogObject.new
+        expect { Logman::Post.esa(log) }.to output("Cannot connect to esa.\n").to_stderr_from_any_process
+      end
+    end
+
+    context 'towards a non-exist logfile' do
+      before do
+        FileUtils.rm_f(filename)
+      end
+
+      it 'should return an error message' do
+        log = Logman::LogObject.new
+        expect { Logman::Post.esa(log) }.to raise_error(Errno::ENOENT)
+      end
+    end
+  end
+
+  describe '#slack' do
+    context 'towards an exist logfile' do
+      before do
+        FileUtils.touch(filename)
+      end
+      after do
+        FileUtils.rm_f(filename)
+      end
+
+      it 'should return a successful message when slack response is valid' do
+        mock_slack_notifier = double('Slack Notifier')
+        allow(mock_slack_notifier).to receive(:post).and_return(true)
+        allow(Logman::Post).to receive(:slack_notifier).and_return(mock_slack_notifier)
+
+        log = Logman::LogObject.new
+        expect { Logman::Post.slack(log) }.to output("Post logfile #{filename} to slack successful.\n").to_stdout
+      end
+
+      it 'should return a unsuccessful message when slack response is invalid' do
+        mock_slack_notifier = double('Slack Notifier')
+        allow(mock_slack_notifier).to receive(:post).and_return(false)
+        allow(Logman::Post).to receive(:slack_notifier).and_return(mock_slack_notifier)
+
+        log = Logman::LogObject.new
+        expect { Logman::Post.slack(log) }.to output("Post logfile #{filename} to slack unsuccessful.\n").to_stdout
+      end
+    end
+    
+    context 'towards a non-exist logfile' do
+      before do
+        FileUtils.rm_f(filename)
+      end
+
+      it 'should return an error message' do
+        log = Logman::LogObject.new
+        expect { Logman::Post.slack(log) }.to raise_error(Errno::ENOENT)
+      end
+    end
+  end
+end
