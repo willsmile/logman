78327d5ea71739b0fadf98db205b3cc51830b265
Wei Chen <smile.v.chen@gmail.com>
2020-08-08 11:03:44 +0900

    Turn config module into a class and revise unit test

--- lib/logman/config.rb
+++ lib/logman/config.rb
@@ -3,20 +3,20 @@
 require 'hashie'
 
 module Logman
-  module Config
+  class Config
     LOGMANPATH = ENV.fetch("LOGMANPATH")
     CONFIG_FILENAME = 'config.yml'
 
-    def self.load(config_path = default_config_path)
+    def initialize(path: nil)
+      @path = path || LOGMANPATH + '/' + CONFIG_FILENAME
+    end
+
+    def load
       begin
-        Hashie::Mash.load(config_path)
+        Hashie::Mash.load(@path)
       rescue ArgumentError
         STDERR.puts 'Please use a valid config path.'
       end
-    end
-
-    def self.default_config_path
-      LOGMANPATH + '/' + CONFIG_FILENAME
     end
   end
 end
