856150ac6e5e198fe01cfdbe6cda8f238c686aaa
Wei Chen <smile.v.chen@gmail.com>
2020-10-19 23:02:52 +0900

    Remove plugins in lib

--- lib/logman.rb
+++ lib/logman.rb
@@ -4,7 +4,6 @@ require 'logman/config'
 require 'logman/log_object'
 require 'logman/post'
 require 'logman/version'
-require 'plugins/logfiles'
 
 module Logman
   class OperationError < StandardError; end
